7f59ff671f1bb7bbce36156c0a8d9d64196511a9
Saurabh Singhal <singhal.saurabh.mnnit@gmail.com>
2016-07-20 00:28:40 +0530

    1. Implement power() function in log(n) to optimize lastKDigits(). 2. Fix the output for the case where lastKDigits() returns lesser than k digit number

--- RiseFallPower.cpp
+++ RiseFallPower.cpp
@@ -5,6 +5,18 @@
 
 using namespace std;
 
+long long int power( int a, int b, int mod ) {
+    if( !b ) {
+        return 1;
+    }
+    long long int temp = power( a, b / 2, mod ) * power( a, b / 2, mod ) % mod;
+    // b is even
+    if( !( b & 1 ) ) {
+        return temp;
+    }
+    return ( temp * a % mod );
+}
+
 // https://discuss.codechef.com/questions/15398/first-k-digits-of-nn
 int firstKDigits( int n, int k ) {
     double x = n * log10( n );
@@ -14,13 +26,9 @@ int firstKDigits( int n, int k ) {
 
 // x = n^n
 // last k digits of x = x % 10^k
-int lastKDigits( int n, int k ) {
-    long long res = 1;
+long long int lastKDigits( int n, int k ) {
     int j = pow( 10, k );
-    for( int i = 1; i <= n; i++ ) {
-        res = ( res * n ) % j;
-    }
-    return res;
+    return power( n, n, j );
 }
 
 int main() {
@@ -30,7 +38,21 @@ int main() {
         int n, k;
         cin >> n >> k;
         cout << firstKDigits( n, k ) << " ";
-        cout << lastKDigits( n, k ) << endl;
+        int lastKDig = lastKDigits( n, k );
+        int tmp = lastKDig;
+        // Handle the case where the length of the returned
+        // integer is less than k. For example, n=10, k=5
+        int len = tmp ? 0 : 1;
+        // Calculate the length of the returned integer
+        while( tmp ) {
+            len++;
+            tmp /= 10;
+        }
+        // Append required number of zeros at the start
+        for( int i = 0; i < ( k - len ); ++i ) {
+            cout << "0";
+        }
+        cout << lastKDig << endl;
     }
     return 0;
 }
