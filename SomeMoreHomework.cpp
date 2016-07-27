17512780f5b4e88c817d3fa683b313743e4efe97
Saurabh Singhal <singhal.saurabh.mnnit@gmail.com>
2016-07-27 20:33:32 +0530

    Fixing generateParityBitString

--- SomeMoreHomework.cpp
+++ SomeMoreHomework.cpp
@@ -5,23 +5,24 @@
 #include <iostream>
 #include <string>
 
-#define MAX 1000000
-
 using namespace std;
 
-void generateParityBitString( string& parityBitString ) {
-    parityBitString += "0110100110010110";
-    string repeating = "1001011001101001";
-    
-    for( int i = 0; i < MAX / 16; ++i ) {
-        parityBitString += repeating;
+const int MAX = 1 << 23;
+string parityBitString = "";
+
+void generateParityBitString() {
+    parityBitString += "0";
+    for( int i = 1; i < MAX; ++i ) {
+        char next = parityBitString[ i >> 1 ];
+        if( i & 1 ) {
+            next ^= 1;
+        }
+        parityBitString += next;
     }
 }
 
 int main() {
-    string parityBitString = "";
-    generateParityBitString( parityBitString );
-
+    generateParityBitString();
     int t;
     cin >> t;
     while( t-- ) {
@@ -36,7 +37,7 @@ int main() {
             } else {
                 given += "0";
             }
-            int pos = parityBitString.find( given );
+            long long int pos = parityBitString.find( given );
             cout << pos;
             if( i == ( len - 1 ) ) {
                 cout << endl;
